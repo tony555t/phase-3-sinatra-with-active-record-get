d9a4101c31f2291efe6f74a30213471efe4d3558
tony555t <tonytullah764@gmail.com>
2023-03-10 13:27:35 +0300

    get request

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,23 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    games = Game.all.order(:title).limit(10)
+    games.to_json
   end
+
+  # use the :id syntax to create a dynamic route
+  get '/games/:id' do
+    # look up the game in the database using its ID
+       # send a JSON-formatted response of the game data
+      game= Game.find(params[:id])
+      game.to_json(only: [:id, :title, :genre, :price], include: {
+        reviews: { only: [:comment, :score], include: {
+          user: { only: [:name] }
+        } }
+      })
+  end
+    
+      end
 
 end
